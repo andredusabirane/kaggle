a7e4fda4fb0c951d4943eb8c91582b818d5669ab
andredusabirane <andredusabirane@gmail.com>
2022-03-06 18:53:29 -0500

    Update KaggleConstant.java

--- Covid-19-Dataset-Load/src/main/java/com/covid/util/KaggleConstant.java
+++ Covid-19-Dataset-Load/src/main/java/com/covid/util/KaggleConstant.java
@@ -2,7 +2,8 @@ package com.covid.util;
 
 public interface KaggleConstant {
 
-	public static final String FILE_PATH = "D:\\workspace\\Kaggle\\Covid-19-Dataset-Load\\Kaggle";
+
+	public static final String FILE_PATH = "D:\\workspace\\Covid-19-Dataset-Load\\Kaggle";
 	public static final String FILE_NAME = "country_wise_latest.csv";
 
 }
